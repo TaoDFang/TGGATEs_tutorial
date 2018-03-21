57ad487cbe6c303638fe962d0b96c325e56466b1
Fang <fangt3@rkalbhpc068.kau.roche.com>
2018-03-21 13:59:11 +0100

    add pathology terms

--- read_data.R
+++ read_data.R
@@ -1,3 +1,4 @@
+setwd("../TGGATEs_tutorial_secrete")
 exprs=read.csv("./data/rat_exprs.csv",header = TRUE,row.names = 1)
 head(exprs[,1:6])
 
@@ -5,15 +6,15 @@ pathology=read.csv("./data/rat_pathology.csv",header = TRUE,row.names = 1)
 # choosing Microgranuloma and "Hypertrophy" 
 
 
-#reorder_index=sample(nrow(pathology))
+reorder_index=sample(nrow(pathology))
 reorder_exprs=exprs[reorder_index,]
 reorder_pathos=pathology[reorder_index,]
 
-pathoes=reorder_pathos[,c("Microgranuloma","Hypertrophy")]
-colSums(pathoes)   # 154, 272
+pathoes=reorder_pathos[,c("Microgranuloma","Increasedmitosis","Hypertrophy")]
+colSums(pathoes)   # 154, 118, 272
 
 pathoes_train=pathoes[1:round(0.8*nrow(pathoes)),]
-colSums(pathoes_train) #121,210
+colSums(pathoes_train) #119,95,207
 
 exprs_train=reorder_exprs[1:round(0.8*nrow(exprs)),]
 exprs_test=reorder_exprs[round(0.8*nrow(exprs)+1):nrow(exprs),]
